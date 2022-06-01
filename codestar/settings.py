4921b74aead6c06c5239d0fcdd7e1e65b9138856
SamMartin92 <samm92@live.ie>
2022-06-01 14:40:33 +0000

    fixes installed apps

--- codestar/settings.py
+++ codestar/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage', # added in after linked to cloudinary
     'django.contrib.staticfiles',
-    'cloudinary' # as above
+    'cloudinary', # as above
     'blog',
 ]
 
