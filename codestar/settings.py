1f6f4ab80b0c1af7ccebcd29278dc2b7f3b42dad
SamMartin92 <samm92@live.ie>
2022-06-03 12:02:17 +0000

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile('env.py'):
     import env
 
@@ -28,7 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['codestar202222.herokuapp.com', 'localhost']
 
@@ -49,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',  # as above
     'django_summernote',
+    'crispy_forms',
     'blog',
 ]
 
@@ -57,6 +61,16 @@ SITE_ID = 1  # added with allauth
 LOGIN_REDIRECT_URL = '/'  # added with allauth
 LOGOUT_REDIRECT_URL = '/'  # added with allauth
 ACCOUNT_EMAIL_VERIFICATION = 'none'  # added with allauth
+
+MESSAGE_TAGS ={
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+
+CRISP_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
